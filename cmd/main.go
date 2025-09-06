3a4c87929466f232cd891b8eb5c73247af147fa7
gera <georgiy.zakharov.98@mail.ru>
2025-09-06 20:37:38 +0300

    add http handler, add task service, add test requests

--- cmd/main.go
+++ cmd/main.go
@@ -10,24 +10,19 @@ import (
 )
 
 func main() {
-	// Создаем роутер Chi
 	r := chi.NewRouter()
 
-	// Подключаем полезные middleware (логирование, сжатие и т.д.)
 	r.Use(middleware.Logger)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
 
+	taskHandler := handler.NewTaskHandler()
+
 	// Регистрируем ОЧЕНЬ точные пути
-	r.Post("/api/send-email", handler.SendEmailHandler) // ТОЛЬКО POST /api/send-email
+	r.Post("/send", taskHandler.SendEmail)
 
 	// Обработчик для корня "/"
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to Gonoty API"))
-	})
-
-	// Обработчик для всего остального (404 Not Found)
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Page not found", http.StatusNotFound)
+		w.Write([]byte("Welcome to Gonoty"))
 	})
 
 	// Запускаем сервер
